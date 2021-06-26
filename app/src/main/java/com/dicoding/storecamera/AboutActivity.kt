7ae443d88baab742c965028995a081287d3dc133
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-06-26 14:44:28 +0700

    Finished update

--- app/src/main/java/com/dicoding/storecamera/AboutActivity.kt
+++ app/src/main/java/com/dicoding/storecamera/AboutActivity.kt
@@ -6,19 +6,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class AboutActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-
-        val btnMove: ImageView = findViewById(R.id.btnBack)
+        val btnMove: ImageView = findViewById(R.id.btnBackAbout)
         btnMove.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.btnBack -> {
+            R.id.btnBackAbout -> {
                 val moveIntent = Intent(this@AboutActivity, MainActivity::class.java)
                 startActivity(moveIntent)
             }
